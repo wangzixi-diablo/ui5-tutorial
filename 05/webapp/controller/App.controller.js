e69ea712fb13cfb1f902eeb11b1fcb7c3adbd9f5
wangzixi-diablo <cle.ee@163.com>
2021-10-08 22:49:30 +0800

    part 5 ok

--- 05/webapp/controller/App.controller.js
+++ 05/webapp/controller/App.controller.js
@@ -2,15 +2,9 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
 	"use strict";
-
-	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
 		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
 			alert("Hello World");
-			/* eslint-enable no-alert */
 		}
 	});
-
 });
--- 05/webapp/index.js
+++ 05/webapp/index.js
@@ -2,9 +2,8 @@ sap.ui.define([
 	"sap/ui/core/mvc/XMLView"
 ], function (XMLView) {
 	"use strict";
-
 	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
+		viewName: "sap.ui5.walkthrough.view.App"
 	}).then(function (oView) {
 		oView.placeAt("content");
 	});
