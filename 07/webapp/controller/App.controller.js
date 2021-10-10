8292b0943a4e0531e7fd5c89558ef2d0b99813a3
wangzixi-diablo <cle.ee@163.com>
2021-10-10 21:38:01 +0800

    add id to input control

--- 07/webapp/controller/App.controller.js
+++ 07/webapp/controller/App.controller.js
@@ -4,18 +4,16 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel"
 ], function (Controller, MessageToast, JSONModel) {
 	"use strict";
-
 	return Controller.extend("sap.ui5.walkthrough.controller.App", {
 		onInit : function () {
 			var oData = {
 				recipient : {
-					name : "World"
+					name : "SAP UI5 初学者教程之七 - JSON 模型初探"
 				}
 			};
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
 		},
-
 		onShowHello : function () {
 			MessageToast.show("Hello World");
 		}
