89bae519e05f940161e1d39dc67a6bfac65b99d5
wangzixi-diablo <cle.ee@163.com>
2021-10-10 10:22:09 +0800

    part 07 ok

---
+++ 07/webapp/controller/App.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+		onInit : function () {
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+
+		onShowHello : function () {
+			MessageToast.show("Hello World");
+		}
+	});
+});
