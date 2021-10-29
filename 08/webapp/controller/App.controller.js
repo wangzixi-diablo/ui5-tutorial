d6f5e42ea2d758c1e53817ba9a9f39b90d287dfd
wangzixi-diablo <cle.ee@163.com>
2021-10-29 16:47:21 +0800

    finish part 8

---
+++ 08/webapp/controller/App.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+	"use strict";
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+		onInit : function () {
+			var oData = {
+				recipient : {
+					name : "SAP UI5 初学者教程之八 - 多语言的支持"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+			var i18nModel = new ResourceModel({
+				bundleName: "sap.ui5.walkthrough.i18n.i18n"
+			 });
+			this.getView().setModel(i18nModel, "jerryi18n");
+		},
+		onShowHello : function () {
+			//MessageToast.show("Hello World"); - 步骤7的代码
+			var oBundle = this.getView().getModel("jerryi18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			// show message
+			MessageToast.show(sMsg);
+		}
+	});
+});
