30b6dcd1dadb07463cc033335224e2f4003a6096
wangzixi-diablo <cle.ee@163.com>
2022-01-10 21:49:09 +0800

    step 14 finished

---
+++ 14/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui5.walkthrough.controller.HelloPanel", {
+       onShowHello : function () {
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          MessageToast.show(sMsg);
+       }
+    });
+ });
