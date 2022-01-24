244264913a109be0f1a7504ce9ebce66990a54b5
wangzixi-diablo <cle.ee@163.com>
2022-01-24 16:33:11 +0800

    part 18 finished

---
+++ 18/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller, MessageToast) {
+	"use strict";
+
+	return Controller.extend("sap.u5.walkthrough.controller.HelloPanel", {
+
+		onShowHello : function () {
+         var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+          MessageToast.show(sMsg);
+		},
+		onOpenDialog : function () {
+
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "sap.ui5.walkthrough.view.HelloDialog"
+				});
+			} 
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		},
+		onCloseDialog : function () {
+			this.byId("helloDialog").close();
+		}
+	});
+});
