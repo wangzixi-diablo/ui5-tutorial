51637db5c7a74c512cd0c87d970cea15c74ba8cb
wangzixi-diablo <cle.ee@163.com>
2022-02-19 19:01:58 +0800

    add 38,39

---
+++ 38/webapp/controller/App.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+	   onShowHello : function () {
+		  var oBundle = this.getView().getModel("i18n").getResourceBundle();
+		  var sRecipient = this.getView().getModel().getproperty("/recipient/name");
+		  var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+		  MessageToast.show(sMsg);
+	   },
+	   onBeforeRendering(){
+        var oView = this.getView();
+        console.log('Jerry App view: ', oView);
+       }
+	});
+ });
