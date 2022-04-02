744295259cc80b564f715fecb1dfac92a1103982
wangzixi-diablo <cle.ee@163.com>
2022-04-02 14:14:16 +0800

    original code

---
+++ 62/localService/mockserver.js
@@ -0,0 +1,38 @@
+sap.ui.define([
+	"sap/ui/core/util/MockServer"
+], function (MockServer) {
+	"use strict";
+
+	return {
+
+		init: function (sODataServiceUrl) {
+			var oMockServer, sLocalServicePath;
+
+			// create
+			oMockServer = new MockServer({
+				rootUri: sODataServiceUrl
+			});
+
+			// configure
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: 10
+			});
+
+			sLocalServicePath = sap.ui.require.toUrl("sap/gantt/sample/GanttChart2OData/localService");
+
+			// simulate
+			oMockServer.simulate(sLocalServicePath + "/metadata.xml", {
+				sMockdataBaseUrl : sLocalServicePath + "/mockdata",
+				bGenerateMissingMockData: true
+			});
+
+			// start
+			oMockServer.start();
+
+			return oMockServer;
+		}
+
+	};
+
+});
