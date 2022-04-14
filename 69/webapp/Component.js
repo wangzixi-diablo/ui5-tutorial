c9263414d7e0c18d6df8661f34019913fd6ac336
wangzixi-diablo <cle.ee@163.com>
2022-04-14 12:22:48 +0800

    code ok

---
+++ 69/webapp/Component.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent"
+], function (UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui5.walkthrough.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this.getRouter().initialize();
+		}
+	});
+});
---
+++ 69/webapp/controller/BaseController.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History",
+	"sap/ui/core/UIComponent"
+], function(Controller, History, UIComponent) {
+	"use strict";
+
+	return Controller.extend("sap.ui5.walkthrough.controller.BaseController", {
+		getRouter : function () {
+			return UIComponent.getRouterFor(this);
+		},
+		onNavBack: function () {
+			var oHistory, sPreviousHash;
+
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("appHome", {}, true);
+			}
+		}
+	});
+});
