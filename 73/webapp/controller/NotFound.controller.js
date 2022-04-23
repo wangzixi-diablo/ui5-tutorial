f00e808724706e8bdb8544850e720b15f821eca5
wangzixi-diablo <cle.ee@163.com>
2022-04-23 14:07:41 +0800

    code ok

---
+++ 73/webapp/controller/NotFound.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+	"sap/ui5/walkthrough/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("sap.ui5.walkthrough.controller.NotFound", {
+
+		onInit: function () {
+			var oRouter, oTarget;
+
+			oRouter = this.getRouter();
+			oTarget = oRouter.getTarget("notFound");
+			oTarget.attachDisplay(function (oEvent) {
+				this._oData = oEvent.getParameter("data");	// store the data
+			}, this);
+		},
+
+		// override the parent's onNavBack (inherited from BaseController)
+		onNavBack : function () {
+			// in some cases we could display a certain target when the back button is pressed
+			if (this._oData && this._oData.fromTarget) {
+				this.getRouter().getTargets().display(this._oData.fromTarget);
+				delete this._oData.fromTarget;
+				return;
+			}
+			// call the parent's onNavBack
+			BaseController.prototype.onNavBack.apply(this, arguments);
+		}
+	});
+});
