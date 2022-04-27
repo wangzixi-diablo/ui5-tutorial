2d54179db28cf6e50a1997060fcec4c28219ecf7
wangzixi-diablo <cle.ee@163.com>
2022-04-27 16:50:36 +0800

    half done

---
+++ 75/webapp/controller/Detail.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui5.walkthrough.controller.Detail", {
+       onShowHello : function () {
+          MessageToast.show('111');
+       }
+    });
+ });
