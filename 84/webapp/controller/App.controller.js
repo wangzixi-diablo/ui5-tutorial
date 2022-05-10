34f1f34d237d78c5be08383c163693647e86ad96
wangzixi-diablo <cle.ee@163.com>
2022-05-10 17:43:45 +0800

    code 84 ok

---
+++ 84/webapp/controller/App.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+	'./BaseController',
+	'sap/ui/model/json/JSONModel'
+], function (BaseController, JSONModel) {
+	"use strict";
+	return BaseController.extend("sap.ui5.walkthrough.controller.App", {
+
+		onInit: function () {
+			var oViewModel = new JSONModel({
+					busy: true,
+					delay: 0
+				});
+
+			this.setModel(oViewModel, "appView");
+
+			this.getOwnerComponent().getModel().metadataLoaded().then(function () {
+				oViewModel.setProperty("/busy", false);
+			});
+		}
+	});
+});
