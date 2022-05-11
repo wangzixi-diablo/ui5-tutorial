f2bf2bea8ac20c13b2a3ddbf3225bc015cd272d8
wangzixi-diablo <cle.ee@163.com>
2022-05-11 16:31:47 +0800

    code ok

---
+++ 86/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/ObjectListItem",
+	"sap/m/ObjectAttribute"
+], function(Controller, ObjectListItem, ObjectAttribute) {
+	"use strict";
+
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+	});
+});
