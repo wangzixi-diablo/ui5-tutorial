febe316b0cd488bc82276deb0fffa5fd18d40d52
wangzixi-diablo <cle.ee@163.com>
2022-09-15 10:38:12 +0800

    129 done

---
+++ 129/webapp/Component.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	'sap/ui/core/UIComponent', 'sap/ui/fl/FakeLrepConnectorLocalStorage'
+], function(UIComponent, FakeLrepConnectorLocalStorage) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui5.walkthrough.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function() {
+			UIComponent.prototype.init.apply(this, arguments);
+		},
+		destroy: function() {
+			FakeLrepConnectorLocalStorage.disableFakeConnector();
+			UIComponent.prototype.destroy.apply(this, arguments);
+		}
+	});
+});
