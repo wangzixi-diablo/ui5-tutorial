323871403a95622ad5ea290f7d94132c1a977fdc
wangzixi-diablo <cle.ee@163.com>
2022-11-02 22:13:30 +0800

    cors error

--- 146/webapp/controller/App.controller.js
+++ 146/webapp/controller/App.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
- ], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/odata/v2/ODataModel"
+ ], function (Controller, ODataModel) {
 	"use strict";
 	return Controller.extend("sap.ui5.walkthrough.controller.App", {
 	   onShowHello : function () {
 		  alert("Hello");
 	   },
 	   onInit: function(){
-		   alert("init");
+		var oModel = new ODataModel("http://services.odata.org/Northwind/Northwind.svc/");
+		oModel.attachMetadataLoaded((data) =>{
+			alert('metadata loaded: ', data);
+		});
 	   }
 	});
  });
