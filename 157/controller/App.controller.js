9e507422bb913dde82691519593e880f52108cdb
wangzixi-diablo <cle.ee@163.com>
2022-12-25 13:39:45 +0800

    remove unused method:

--- 157/controller/App.controller.js
+++ 157/controller/App.controller.js
@@ -1,32 +1,20 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/vk/ContentResource",
-	"sap/m/MessageToast"
-], function(Controller, JSONModel, ContentResource, MessageToast) {
+	"sap/ui/vk/ContentResource"
+], function(Controller, JSONModel, ContentResource) {
 	"use strict";
 
-	var handleEmptyUrl = function(view) {
-		var oBundle = view.getModel("i18n").getResourceBundle();
-		var msg = oBundle.getText("missingUrl");
-		MessageToast.show(msg);
-	};
-
-	var loadModelIntoViewer = function(viewer, remoteUrl, sourceType, localFile) {
-		// what is currently loaded in the view is destroyed
+	var loadModelIntoViewer = function(viewer, sourceType, localFile) {
 		viewer.destroyContentResources();
-
-		var source = remoteUrl || localFile;
-
+		var source = localFile;
+		
 		if (source) {
-			// content of viewer is replaced with new data
 			var contentResource = new ContentResource({
 				source: source,
 				sourceType: sourceType,
 				sourceId: "abc"
 			});
-
-			// content: chosen path. content added to the view
 			viewer.addContentResource(contentResource);
 		}
 	};
@@ -36,52 +24,30 @@ sap.ui.define([
 	onInit: function() {
 		var sourceData = {
 			localFile: undefined,
-				remoteUrl: undefined
-			};
-			var model = new JSONModel();
-			model.setData(sourceData);
-			this.getView().setModel(model, "source");
-		},
+			remoteUrl: undefined
+		};
+		
+		var model = new JSONModel();
+		model.setData(sourceData);
+		this.getView().setModel(model, "source");
+	},
 
-		onPressLoadRemoteModel: function(event) {
-			var view = this.getView();
-			var sourceData = view.getModel("source").oData;
-			var viewer = view.byId("viewer");
-			if (sourceData.remoteUrl) {
-				loadModelIntoViewer(viewer, sourceData.remoteUrl, "vds");
-			} else {
-				handleEmptyUrl(view);
-			}
-		},
+	onPressLoadSampleFile: function(event) {
+		var view = this.getView();
+		var viewer = view.byId("viewer");
+		loadModelIntoViewer(viewer, "vds","models/test.vds");
+	},
 
-		onPressLoadRemoteImage: function(event) {
-			var view = this.getView();
-			var sourceData = view.getModel("source").oData;
-			var viewer = view.byId("viewer");
-			if (sourceData.remoteUrl) {
-				loadModelIntoViewer(viewer, sourceData.remoteUrl, "jpg");
-			} else {
-				handleEmptyUrl(view);
-			}
-		},
-
-		onPressLoadSampleFile: function(event) {
-			var view = this.getView();
-			var viewer = view.byId("viewer");
-			loadModelIntoViewer(viewer, "models/cooper.vds", "vds");
-		},
-
-		onChangeFileUploader: function(event) {
+	onChangeFileUploader: function(event) {
 			var view = this.getView();
 			var viewer = view.byId("viewer");
 			var localFile = event.getParameter("files")[0];
-			// if user selects a local file
 			if (localFile) {
 				var fileName = localFile.name;
 				var index = fileName.lastIndexOf(".");
 				if (index >= 0 && index < fileName.length - 1) {
 					var sourceType = fileName.substr(index + 1);
-					loadModelIntoViewer(viewer, null, sourceType, localFile);
+					loadModelIntoViewer(viewer, sourceType, localFile);
 				}
 			}
 		}
