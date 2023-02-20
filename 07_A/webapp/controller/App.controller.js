66ddd61136555ad79996d9fc7c6e8813a6aaae68
wangzixi-diablo <cle.ee@163.com>
2023-02-20 15:31:47 +0800

    add compatible version

---
+++ 07_A/webapp/controller/App.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+	"use strict";
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+		onInit : function () {
+			var oData = {
+				recipient : {
+					name : "SAP UI5 初学者教程之七 - JSON 模型初探"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
+		onShowHello : function () {
+			MessageToast.show("Hello World");
+			var oInput = this.byId("myinput");
+			console.log(oInput);
+		}
+	});
+});
