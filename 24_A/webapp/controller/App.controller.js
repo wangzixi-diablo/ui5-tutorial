94c12f3fcfbfd707468efd1752550de174e9a977
wangzixi-diablo <cle.ee@163.com>
2023-05-04 11:37:09 +0800

    fix error that proxy middleware is not found

---
+++ 24_A/webapp/controller/App.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+	"use strict";
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+	});
+ });
