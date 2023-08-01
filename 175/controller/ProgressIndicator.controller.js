cad75fd0126316398c62b893792a83187715777c
wangzixi-diablo <cle.ee@163.com>
2023-08-01 10:54:57 +0800

    progress bar

---
+++ 175/controller/ProgressIndicator.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+		'sap/ui/core/mvc/Controller',
+		'sap/ui/model/json/JSONModel'
+	], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui5.walkthrough.controller.ProgressIndicator", {
+		onPIChangeValueButtonPressed : function (oEvent) {
+			var sSourceId = oEvent.getSource().getId(),
+				sButton = 'button',
+				iIndexOfButton = sSourceId.indexOf(sButton),
+				oProgressIndicator = this.getView().byId(sSourceId.substring(0, iIndexOfButton - 1)),
+				sValue = sSourceId.substring(iIndexOfButton + sButton.length);
+
+			oProgressIndicator.setDisplayValue(sValue + '%');
+			oProgressIndicator.setPercentValue(+sValue);
+		}
+	});
+});
