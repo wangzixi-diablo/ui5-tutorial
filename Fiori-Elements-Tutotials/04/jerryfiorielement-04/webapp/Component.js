6857e7fd3842fe32b1a51de3e5dbf4522f64b4fb
wangzixi-diablo <cle.ee@163.com>
2023-08-19 11:02:35 +0800

    extension api initial upload

---
+++ Fiori-Elements-Tutotials/04/jerryfiorielement-04/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("jerryfiorielement03.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
