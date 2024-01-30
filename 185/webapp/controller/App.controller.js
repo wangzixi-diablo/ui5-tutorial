663be3cd30d1734fbaae572282faa98d0e04deaa
wangzixi-diablo <cle.ee@163.com>
2024-01-30 16:09:00 +0800

    add colorful console.log

---
+++ 185/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+	return Controller.extend("sap.ui5.walkthrough.controller.App", {
+		onShowHello : function () {
+			console.log("Hello World");
+		}
+	});
+});
