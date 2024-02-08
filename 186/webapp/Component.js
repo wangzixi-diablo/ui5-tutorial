276d3d76fa508191ad7dc3a9dec8be157328abfe
wangzixi-diablo <cle.ee@163.com>
2024-02-08 13:16:39 +0800

    add multiple selection for SmartTable

---
+++ 186/webapp/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	'sap/ui/core/UIComponent'
+], function(UIComponent) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui5.walkthrough.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
