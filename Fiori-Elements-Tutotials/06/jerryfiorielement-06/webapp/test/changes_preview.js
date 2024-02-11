23c75a7d1164aac77784a980e67d4c3a3d7bb8f0
wangzixi-diablo <cle.ee@163.com>
2024-02-11 21:14:38 +0800

    06 cloned ok

--- Fiori-Elements-Tutotials/06/jerryfiorielement-06/webapp/test/changes_preview.js
+++ Fiori-Elements-Tutotials/06/jerryfiorielement-06/webapp/test/changes_preview.js
@@ -163,7 +163,7 @@ if (parseInt(version[0], 10) <= 1 && parseInt(version[1], 10) < 78) {
                                             oResult.changes = aProcessedChanges;
                                             var oLrepChange = {
                                                 changes: oResult,
-                                                componentClassName: "jerryfiorielement01"
+                                                componentClassName: "jerryfiorielement06"
                                             };
                                             resolve(oLrepChange);
                                         });
@@ -174,7 +174,7 @@ if (parseInt(version[0], 10) <= 1 && parseInt(version[1], 10) < 78) {
                                         oResult.changes = aProcessedChanges;
                                         var oLrepChange = {
                                             changes: oResult,
-                                            componentClassName: "jerryfiorielement01"
+                                            componentClassName: "jerryfiorielement06"
                                         };
                                         resolve(oLrepChange);
                                     }
