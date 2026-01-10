e380fd21bc2d0d553008bc445ca74916e1ca8799
wangzixi-diablo <cle.ee@163.com>
2026-01-10 21:15:50 +0800

    popup works

--- 05_F/webapp/controller/App.controller.js
+++ 05_F/webapp/controller/App.controller.js
@@ -1,15 +1,31 @@
+
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/library",
+	"sap/m/MessageToast"
+	], function (Controller, Library, MessageToast) {
 	"use strict";
 	return Controller.extend("sap.ui5.walkthrough.controller.App", {
-		onShowHello : function () {
-			alert("Hello World");
-		},
-
 		// Opens SAP website in a new browser window/tab
 		onOpenSap: function () {
-			window.open("https://www.sap.com", "_blank", "noopener,noreferrer");
+			// Use URLHelper to safely open external URL in a new tab
+			Library.URLHelper.redirect("https://www.sap.com", true);
+		}
+		,
+		// Opens Baidu in a popup window
+		onOpenBaiduPopup: function () {
+			// Try to open a popup window with reasonable features
+			var popup = window.open("https://www.baidu.com", "_blank", "noopener,noreferrer,width=1024,height=768,scrollbars=yes,resizable=yes");
+			if (!popup) {
+				MessageToast.show("Popup blocked by the browser. Please allow popups for this site.");
+				return;
+			}
+			try {
+				// For security, explicitly set noopener/noreferrer when possible
+				popup.opener = null;
+			} catch (e) {
+				// Ignore if the browser prevents access
+			}
 		}
 	});
 });
